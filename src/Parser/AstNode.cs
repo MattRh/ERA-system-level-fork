87dd2d653d3e952ffc292393c8aa01adabb60ede
Sergey <sergejrf@gmail.com>
2019-01-22 23:04:14 +0300

    Added astnode

--- src/Parser/AstNode.cs
+++ src/Parser/AstNode.cs
@@ -1,7 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using src.Tokenizer;
+
 namespace src.Parser
 {
     public class AstNode
     {
-        
+        public readonly NodeType Type;
+        public readonly string Value;
+        public readonly Token Token;
+
+        public readonly List<AstNode> Children = new List<AstNode>();
+
+        private int? _numeric;
+
+        AstNode(NodeType type, string value)
+        {
+            this.Type = type;
+            this.Value = value;
+        }
+
+        AstNode(NodeType type, Token token) : this(type, token.Value)
+        {
+            this.Token = token;
+        }
+
+        public int NumericValue()
+        {
+            if (Type != NodeType.Literal)
+            {
+                throw new ArgumentException("Can't get numeric value from non-literal node");
+            }
+
+            if (_numeric == null)
+            {
+                _numeric = int.Parse(Value);
+            }
+
+            return (int) _numeric;
+        }
+
+        public override string ToString()
+        {
+            var currentName = Type.ToString();
+            if (!string.IsNullOrEmpty(Value) && !currentName.Equals(Value))
+            {
+                currentName += "(" + Value + ")";
+            }
+
+            var childrenJson = "";
+            foreach (var i in Children)
+            {
+                var childrenList = i.ToString().Split('\n');
+                var formattedChildren = childrenList.Aggregate("", (current, j) => $"{current} {j}\n");
+
+                childrenJson = childrenJson + formattedChildren;
+            }
+
+            var currentJson = currentName;
+            if (childrenJson.Length > 0)
+            {
+                currentJson += ": {\n" + childrenJson + "}";
+            }
+
+            return currentJson;
+        }
     }
 }
